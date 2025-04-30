5b7ff61aa63109748b2f6a2fcd536371cb3fa0f8
yamazakihirofumi <dourlin@outlook.com>
2025-04-30 16:08:27 -0400

    update install

--- install_env.sh
+++ install_env.sh
@@ -1,3 +1,8 @@
+#In case conda is not instlled
+#
+#curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh; chmod +x; miniconda.sh; sh miniconda.sh -b -p /content/miniconda
+#export PATH="/content/miniconda/bin:$PATH"
+
 conda create -n finetune python=3.10
 
 conda activate finetune
